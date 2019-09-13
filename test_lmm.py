07678d96736d623e9562bd8d9746ad646a50cc3f
lukepinkel <lukepinkel@gmail.com>
2019-09-13 15:54:52 -0700

    Add Tests

--- test_lmm.py
+++ test_lmm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep 11 23:30:03 2019
+Created on Fri Sep 13 15:45:36 2019
 
 @author: lukepinkel
 """
@@ -10,23 +10,23 @@ import pandas as pd
 import numpy as np
 
 import seaborn as sns
-import mvt.api as mva
+from mvpy.api import LMM
 import matplotlib.pyplot as plt
 data = pd.read_csv("/users/lukepinkel/Downloads/wls2.csv", index_col=0)
 data = data.drop('idpub.1', axis=1)
 
-olsA = mva.LM("IQ~1+C(Religiosity)*C(sexrsp)", data=data)
+olsA = LM("IQ~1+C(Religiosity)*C(sexrsp)", data=data)
 olsA.sumstats
 olsA.beta
 
 
-olsB = mva.LM("attract~1+C(Religiosity)*C(sexrsp)", data=data)
+olsB = LM("attract~1+C(Religiosity)*C(sexrsp)", data=data)
 olsB.sumstats
 olsB.beta
 
 
 
-olsC = mva.LM("attract~1+C(Extraversion)*C(sexrsp)+IQ", data=data)
+olsC = LM("attract~1+C(Extraversion)*C(sexrsp)+IQ", data=data)
 olsC.sumstats
 olsC.beta
 X = olsC.X
