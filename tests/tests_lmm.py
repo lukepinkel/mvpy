97f841fd3737095803c6c0a1a9ca1ffe80144b61
lukepinkel <lukepinkel@gmail.com>
2019-09-14 19:25:57 -0700

    Create tests_lmm.py

---
+++ tests/tests_lmm.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 14 19:23:30 2019
+
+@author: lukepinkel
+"""
+
+import statsmodels.api as sma
+from mvpy.api import LMM
+import numpy as np
+import seaborn as sns
+
+data = sma.datasets.get_rdataset('dietox', 'geepack').data.dropna()
+
+fe = "~Time"
+re = {"Pig":"~1"}
+y = "Weight"
+
+lmm_mod = LMM(fe, re, y, data)
+lmm_mod.fit()
+
+lmm_mod.params
+lmm_mod.b
+lmm_mod.res
+mlm_mod = sma.formula.mixedlm("Weight~Time", re_formula="~1", groups="Pig",
+                              data=data).fit()
+
+mlm_mod.summary()
+np.linalg.norm(data['Weight'] - mlm_mod.predict())
+np.linalg.norm(data['Weight'] - mlm_mod.predict())
+
+fe = "~Time"
+re = {"Pig":"~1+Time"}
+y = "Weight"
+
+lmm_mod = LMM(fe, re, y, data)
+lmm_mod.fit()
+
+lmm_mod.params
+np.linalg.norm(lmm_mod.y - lmm_mod.X.dot(lmm_mod.b))
+np.linalg.norm(lmm_mod.y - lmm_mod.X.dot(lmm_mod.b) -lmm_mod.Z.dot(lmm_mod.u))
+
+
+
+fe = "~Time+Feed"
+re = {"Pig":"~1+Time", "Litter":"~1"}
+y = "Weight"
+
+lmm_mod = LMM(fe, re, y, data)
+lmm_mod.fit()
+
+lmm_mod.params
+np.linalg.norm(lmm_mod.y - lmm_mod.X.dot(lmm_mod.b))
+np.linalg.norm(lmm_mod.y - lmm_mod.X.dot(lmm_mod.b) -lmm_mod.Z.dot(lmm_mod.u))
+
+sns.jointplot(lmm_mod.y, lmm_mod.X.dot(lmm_mod.b))
+sns.jointplot(lmm_mod.y, lmm_mod.X.dot(lmm_mod.b)+lmm_mod.Z.dot(lmm_mod.u))
+
+
