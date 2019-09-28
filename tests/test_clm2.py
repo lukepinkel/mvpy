d2ec74b0539b0d2423eaa3079f418f64cf7b474c
Luke Pinkel <lukepinkel@Lukes-MacBook-Pro.local>
2019-09-27 17:49:49 -0700

    Update test_clm2.py

--- tests/test_clm2.py
+++ tests/test_clm2.py
@@ -8,22 +8,16 @@ Created on Fri Sep 20 18:51:25 2019
 
 import pandas as pd
 import numpy as np
-from mvpy.api import CLM
-from scipy.stats import chi2 as chi2_dist
+import mvpy.models.clm as clm
+import importlib
+importlib.reload(clm)
+
 
 data = pd.read_csv("/users/lukepinkel/Downloads/wine.csv", index_col=0)
 data['temp'] = data['temp'].replace({'cold':0, 'warm':1})
 data['contact'] = data['contact'].replace({'no':0, 'yes':1})
 
-clm_mod = CLM("rating ~ temp+contact-1", data=data)
+clm_mod = clm.CLM("rating ~ temp+contact-1", data=data)
 clm_mod.fit()
 clm_mod.res
 
-clm_int  =CLM("rating ~ 1", data=data)
-clm_int.fit()
-
-LLN = clm_int.loglike(clm_int.params)
-LLF = clm_mod.loglike(clm_mod.params)
-
-LLR = LLN - LLF
-LLRp = chi2_dist.sf(LLR, len(clm_mod.params)-1)
