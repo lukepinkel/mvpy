4c793ffdaf2fd979bb6ab8b50a43187a97de38f0
Luke Pinkel <lukepinkel@Lukes-MacBook-Pro.local>
2019-10-02 08:00:34 -0700

    Update mv_rand.py

--- models/mv_rand.py
+++ models/mv_rand.py
@@ -8,11 +8,7 @@ Created on Wed Sep 11 19:04:52 2019
 
 import numpy as np
 import pandas as pd
-from numpy import dot, eye, diag, sqrt, zeros
-from numpy.linalg import cholesky, inv, multi_dot
-
-from ..utils.linalg_utils import whiten, normalize_diag, sorted_eigh, mdot, chol
-from ..utils.base_utils import check_type
+from ..utils import linalg_utils, base_utils
 
 
 def multi_rand(R, size=1000):
@@ -27,13 +23,13 @@ def multi_rand(R, size=1000):
     Returns:
         Y: size by n matrix of multivariate random normal values
     '''
-    R, col, ix, is_pd = check_type(R)
+    R, col, ix, is_pd = base_utils.check_type(R)
     
     n = R.shape[0]
-    X = whiten(np.random.rand(size, n))
+    X = linalg_utils.whiten(np.random.rand(size, n))
     
-    L = chol(R)
-    Y = dot(L, X.T).T
+    L = linalg_utils.chol(R)
+    Y = np.dot(L, X.T).T
     if is_pd:
         Y = pd.DataFrame(Y, columns=col)
     return Y
@@ -51,28 +47,50 @@ def random_correlations(n_feats, n_obs=10):
         R: random correlation matrix
     '''
     W = np.random.randn(n_obs, n_feats)
-    R = dot(W.T, W) + diag(np.random.rand(n_feats))
-    D = diag(1/sqrt(diag(R)))
-    R = multi_dot([D, R, D])
+    R = np.dot(W.T, W) + np.diag(np.random.rand(n_feats))
+    D = np.diag(1.0/np.sqrt(np.diag(R)))
+    R = np.linalg.multi_dot([D, R, D])
     return R
 
        
 def vine_corr(d, betaparams):
-    P = zeros((d, d))
-    S = eye(d)
+    P = np.zeros((d, d))
+    S = np.eye(d)
     for k in range(d-1):
         for i in range(k+1, d):
             P[k, i] = np.random.beta(betaparams, betaparams)
             P[k, i] = (P[k, i] - 0.5)*2.0
             p = P[k, i]
             for l in range(k-1, 1, -1):
-                p = p * sqrt((1 - P[l, i]**2)*(1 - P[l, k]**2)) + P[l, i]*P[l, k]
+                p = p * np.sqrt((1 - P[l, i]**2)*(1 - P[l, k]**2)) + P[l, i]*P[l, k]
             S[k, i] = p
             S[i, k] = p
-    u, V = sorted_eigh(S)
+    u, V = linalg_utils.sorted_eigh(S)
     umin = np.min(u[u>0])
     u[u<0] = [umin*0.5**(float(i+1)/len(u[u<0])) for i in range(len(u[u<0]))]
-    S = mdot([V, diag(u), V.T])
-    S = normalize_diag(S)
+    S = linalg_utils.mdot([V, np.diag(u), V.T])
+    S = linalg_utils.normalize_diag(S)
+    return S
+
+ 
+def onion_corr(d, betaparams=5):
+    S = np.eye(d)
+    S[0, 1] = 2*np.random.beta(betaparams, betaparams)-1.0
+    S[1, 0] = S[0, 1]
+    r = S[:2, :2]
+    for k in range(2, d-1):
+        y = np.sqrt(np.random.beta(k/2.0, betaparams))
+        u = np.random.rand(k)
+        u /= np.linalg.norm(u)
+        u *= y
+        A = np.linalg.cholesky(r)
+        z = A.dot(u)[:, None]
+        r = np.block([[r, z], [z.T, np.ones((1, 1))]])
+    S = r
+    u, V = linalg_utils.sorted_eigh(S)
+    umin = np.min(u[u>0])
+    u[u<0] = [umin*0.5**(float(i+1)/len(u[u<0])) for i in range(len(u[u<0]))]
+    S = linalg_utils.mdot([V, np.diag(u), V.T])
+    S = linalg_utils.normalize_diag(S)
     return S
         
