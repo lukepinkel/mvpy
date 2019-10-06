4159b4428b01eda87f63ab95812c5b42a563903b
Luke Pinkel <lukepinkel@Lukes-MacBook-Pro.local>
2019-10-06 14:38:38 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="mvpy",
+    version="0.0.1",
+    url="https://github.com/lukepinkel/mvpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License ::  MIT License",
+    ],
+    python_requires='>=2.7',
+)
