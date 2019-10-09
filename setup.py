461a5e969a378983eee58f1e67d29385179a0283
Luke Pinkel <lukepinkel@Lukes-MacBook-Pro.local>
2019-10-09 00:05:19 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="mvpy",
-    version="0.1.3",
+    version="0.1.4",
     url="https://github.com/lukepinkel/mvpy",
     packages=setuptools.find_packages(),
     classifiers=[
