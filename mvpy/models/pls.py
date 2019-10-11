76b6699d0f8c892f80d58dc378d4b16c0018ffc7
Luke Pinkel <lukepinkel@Lukes-MacBook-Pro.local>
2019-10-10 19:25:57 -0700

    update pls.py

--- mvpy/models/pls.py
+++ mvpy/models/pls.py
@@ -490,7 +490,9 @@ class PLSR:
                                                    self.ncomps)
             elif method == 'W2A':
                 _, _, XL, YL, B = pls_w2a(X_samples, Y_samples, self.ncomps)
-            rss += np.mean((Y_test - X_test.dot(B))**2)/n_samples
+            ssr  = np.sum((Y_test - X_test.dot(B))**2)
+            r2 = 1 - ssr / np.sum((Y_test - Y_test.mean())**2)
+            rss += r2/n_samples
         self.crossval_rss = rss
         
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="mvpy",
-    version="0.2.2",
+    version="0.2.3",
     url="https://github.com/lukepinkel/mvpy",
     packages=setuptools.find_packages(),
     classifiers=[
