13de530d188d988abb7b1615427661539d76902a
Luke Pinkel <lukepinkel@Old-Meme-Bro.local>
2019-11-05 00:03:10 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="mvpy",
-    version="0.5.9",
+    version="0.6.0",
     url="https://github.com/lukepinkel/mvpy",
     packages=setuptools.find_packages(),
     classifiers=[
