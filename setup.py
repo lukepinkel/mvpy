306e7bae634ce532aaa30ed44335f625696b7410
Luke Pinkel <lukepinkel@China-NB1.local>
2020-01-19 01:26:53 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="mvpy",
-    version="0.7.5",
+    version="0.7.6",
     url="https://github.com/lukepinkel/mvpy",
     packages=setuptools.find_packages(),
     classifiers=[
